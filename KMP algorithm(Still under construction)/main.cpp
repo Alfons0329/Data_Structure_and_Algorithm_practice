91e16c5efeaacb13f9a07fa4afe7b3487dc5f21b
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2016-10-11 17:58:30 +0800

    Main function

---
+++ KMP algorithm(Still under construction)/main.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#include<string>
+#include<stack>
+#include<set>
+#include<map>
+#include<queue>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"KMP function.h" 
+using namespace std;
+
+int main()
+{
+	kmp_functions kmp;
+	system("pause");
+	return 0;
+}
