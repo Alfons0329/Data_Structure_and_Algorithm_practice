16a5dd3cc2f7f11117ca5b6c28dd88af86708f8a
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2016-11-17 00:34:41 +0800

    Still under debugging

---
+++ Equivalence Class Algorithm/With disjoint tree/Union and find eqc header.h
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class equi_class
+{
+public:
+	equi_class()
+	{
+		input();
+	}
+	void input();
+	void initlialize_itself_as_parent();
+	void union_find();
+	void trace_origin(int);
+	void print_all_equi_class(); 
+	vector<int> parent;
+};
+
+void equi_class::input()
+{
+	int cnt,tmpx,tmp_parent;
+	cout << "Num of elements: ";
+	cin >> cnt;
+	parent.resize(cnt);
+	cout << "Num of relations: ";
+	cin >>cnt;
+	initlialize_itself_as_parent();
+	for (int i = 0; i < cnt; i++)
+	{
+		cout << "Enter the element and its parent: ";
+		cin >> tmpx >> tmp_parent;
+		if (tmp_parent > tmpx)
+			swap(tmp_parent, tmpx);
+
+		parent[tmpx] = tmp_parent;
+	}
+	
+	union_find();
+	print_all_equi_class();
+}
+void equi_class::initlialize_itself_as_parent()
+{
+	for (int i = 0; i < parent.size(); i++)
+	{
+		parent[i] = i;
+	}
+}
+void equi_class::union_find()
+{
+	for (int i = 0; i < parent.size(); i++)
+	{
+		trace_origin(i);
+	}
+}
+void equi_class::trace_origin(int in)
+{
+	int leader,in_parent=parent[in]; //store such as 1-->5-->3-->2 we store 1
+	while (in != parent[in])
+	{
+		in = parent[in];
+	}
+	leader = in;
+	while (parent[in_parent] != leader) //collapsing rule
+	{
+		in_parent = parent[in_parent];
+		parent[in_parent] = leader;
+	}
+
+	
+}
+void equi_class::print_all_equi_class()
+{
+	cout << "Equivalence classes are: ";
+	for (int i = 0; i < parent.size(); i++)
+	{
+		if (parent[i]>-999)
+		{
+			for (int j = i; j < parent.size(); j++)
+			{
+				if (parent[j] == parent[i])
+				{
+					cout << j << " ";
+					parent[j] = -1000;
+				}
+			}
+		}
+		
+		cout << endl;
+	}
+	system("pause");
+}
